1ab8c777732aff64f745553b393e522d197cfa24
Dicki Fathurohman <100754802+dickifathurohman@users.noreply.github.com>
2024-11-23 08:41:38 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -164,7 +164,7 @@ def plot_weekday_usage(df):
 # MAIN 
 
 #load data
-df = pd.read_csv("/main_data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/dickifathurohman/submission_analisis_data/main/dashboard/main_data.csv")
 
 #mengubah tipe data
 df["dteday"] = pd.to_datetime(df["dteday"])
